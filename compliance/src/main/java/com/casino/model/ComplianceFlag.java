bc201f3899b84dcf837f9828d5f6525776dbd863
Heigo Tornik <heigotornik@gmail.com>
2026-05-03 23:54:53 +0300

    Add repo methods for flag

--- compliance/src/main/java/com/casino/model/ComplianceFlag.java
+++ compliance/src/main/java/com/casino/model/ComplianceFlag.java
@@ -3,14 +3,14 @@ package com.casino.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.OffsetDateTime;
 
 @Data
 @Entity
-@AllArgsConstructor
+@NoArgsConstructor
 public class ComplianceFlag {
     @Id
     private Long flagId;
--- compliance/src/main/java/com/casino/repository/ComplianceFlagRepository.java
+++ compliance/src/main/java/com/casino/repository/ComplianceFlagRepository.java
@@ -1,7 +1,16 @@
 package com.casino.repository;
 
 import com.casino.model.ComplianceFlag;
+import com.casino.model.ComplianceFlagSeverity;
+import com.casino.model.ComplianceFlagType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ComplianceFlagRepository extends JpaRepository<ComplianceFlag, Long> {
+    Optional<ComplianceFlag> findByFlagIdAndComplianceId(Long flagId, Long complianceId);
+
+    List<ComplianceFlag> findByComplianceIdAndTypeAndSeverity(
+        Long complianceId, ComplianceFlagType type, ComplianceFlagSeverity severity);
 }
