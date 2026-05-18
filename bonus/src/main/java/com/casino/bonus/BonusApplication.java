8d95274fc3171cd473edcf4943b6858f5674eb17
Joonas Tiitson <joonastiitson@gmail.com>
2026-05-18 23:18:20 +0300

    game service discovery thing

--- bonus/src/main/java/com/casino/bonus/BonusApplication.java
+++ bonus/src/main/java/com/casino/bonus/BonusApplication.java
@@ -2,7 +2,10 @@ package com.casino.bonus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;               
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.web.client.RestTemplate;               
 
 @EnableKafka
 @SpringBootApplication
@@ -10,5 +13,12 @@ public class BonusApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BonusApplication.class, args);
+    }
+
+    // ONLY add this if the bonus service needs to call another service via http://service-name
+    @Bean
+    @LoadBalanced 
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
